31cbd058e2d1419b1a966d4ddaafdd46fa4353cd
MauroRaya <maurorayabr@gmail.com>
2025-12-27 22:50:57 -0300

    LMAO why can you @ someone in a commit message, sorry about that man

--- src/main.zig
+++ src/main.zig
@@ -1,18 +1,23 @@
 const std = @import("std");
 const heap = std.heap;
 const fs = std.fs;
-const io = std.io;
+const mem = std.mem;
+const math = std.math;
 
 pub fn main() !void {
     var gpa = heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const alloc = gpa.allocator();
 
-    const file = try fs.cwd().openFile("test.txt", .{});
-    defer file.close();
-
-    const buf = try file.readToEndAlloc(alloc, std.math.maxInt(usize));
+    const buf = try readFile(alloc, "test.txt");
     defer alloc.free(buf);
 
     std.debug.print("{s}", .{buf});
+}
+
+// https://ziggit.dev/t/how-to-read-an-entire-text-file-line-by-line-into-memory-efficiently/6433/4
+pub fn readFile(alloc: mem.Allocator, path: []const u8) ![]u8 {
+    const file = try fs.cwd().openFile(path, .{});
+    defer file.close();
+    return file.readToEndAlloc(alloc, math.maxInt(usize));
 }
