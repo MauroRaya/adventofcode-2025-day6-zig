42bfcdb1ee2edc91d625c2a5960547b156392861
MauroRaya <maurorayabr@gmail.com>
2025-12-28 13:38:32 -0300

    fix: create arrayList of iterators

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const heap = std.heap;
 const fs = std.fs;
+const ArrayList = std.ArrayList;
 const mem = std.mem;
 const math = std.math;
 
@@ -12,7 +13,21 @@ pub fn main() !void {
     const buf = try readFile(alloc, "test.txt");
     defer alloc.free(buf);
 
-    std.debug.print("{s}", .{buf});
+    var iterators = try ArrayList(mem.SplitIterator(u8, mem.DelimiterType.any)).initCapacity(alloc, 1024);
+    defer iterators.deinit(alloc);
+
+    var lines = mem.splitAny(u8, buf, "\n");
+
+    while (lines.next()) |line| {
+        try iterators.append(alloc, mem.splitAny(u8, line, " "));
+    }
+
+    for (iterators.items) |*iterator| {
+        while (iterator.next()) |char| {
+            std.debug.print("{s} ", .{char});
+        }
+        std.debug.print("\n", .{});
+    }
 }
 
 // https://ziggit.dev/t/how-to-read-an-entire-text-file-line-by-line-into-memory-efficiently/6433/4
