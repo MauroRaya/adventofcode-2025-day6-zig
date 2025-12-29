b86662fce861073277ce66a4a7f2dd92b263de8b
MauroRaya <maurorayabr@gmail.com>
2025-12-28 21:47:21 -0300

    feat: transform iterators into slices for part 2 of the problem, i'll refactor later some stuff

--- src/main.zig
+++ src/main.zig
@@ -1,87 +1,108 @@
-const std: type = @import("std");
-
-const mem: type = std.mem;
-const Allocator: type = mem.Allocator;
-const SplitIterator = mem.SplitIterator;
-const DelimiterType: type = mem.DelimiterType;
-
-const heap: type = std.heap;
-const DebugAllocator = heap.DebugAllocator;
+const std = @import("std");
+const mem = std.mem;
+const Allocator = mem.Allocator;
+const heap = std.heap;
 const GeneralPurposeAllocator = heap.GeneralPurposeAllocator;
-
-const fs: type = std.fs;
-const File: type = fs.File;
-
+const fs = std.fs;
 const ArrayList = std.ArrayList;
-const math: type = std.math;
+const math = std.math;
+const fmt = std.fmt;
 
-pub fn add(list: ArrayList(i32)) i32 {
-    var total: i32 = 0;
+pub fn add(list: ArrayList(u64)) u64 {
+    var total: u64 = 0;
     for (list.items) |num| {
         total += num;
     }
     return total;
 }
 
-pub fn multiply(list: ArrayList(i32)) i32 {
-    var total: i32 = 1;
+pub fn multiply(list: ArrayList(u64)) u64 {
+    var total: u64 = 1;
     for (list.items) |num| {
         total *= num;
     }
     return total;
 }
 
-// https://ziggit.dev/t/how-to-read-an-entire-text-file-line-by-line-into-memory-efficiently/6433/4
 pub fn readFile(alloc: Allocator, path: []const u8) ![]u8 {
-    const file: File = try fs.cwd().openFile(path, .{});
+    const file = try fs.cwd().openFile(path, .{});
     defer file.close();
     return file.readToEndAlloc(alloc, math.maxInt(usize));
 }
 
-pub fn main() !void {
-    var gpa: DebugAllocator(.{}) = GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-    const alloc: Allocator = gpa.allocator();
+pub fn sum(alloc: Allocator, input: []u8) !u64 {
+    var line_slices = try ArrayList(ArrayList([]const u8)).initCapacity(alloc, 1024);
+    defer {
+        for (line_slices.items) |*slice| {
+            slice.deinit(alloc);
+        }
+        line_slices.deinit(alloc);
+    }
 
-    const buf: []u8 = try readFile(alloc, "test.txt");
-    defer alloc.free(buf);
+    var lines_iter = mem.splitScalar(u8, input, '\n');
 
-    var lines: SplitIterator(u8, DelimiterType.scalar) = mem.splitScalar(u8, buf, '\n');
-
-    const LineIterator = mem.TokenIterator(u8, mem.DelimiterType.scalar);
-    var lineIts = try ArrayList(LineIterator).initCapacity(alloc, 1024);
-    defer lineIts.deinit(alloc);
-
-    while (lines.next()) |line| {
+    while (lines_iter.next()) |line| {
         if (line.len == 0) continue;
 
-        const it = mem.tokenizeScalar(u8, line, ' ');
-        try lineIts.append(alloc, it);
-    }
+        var line_slice = try ArrayList([]const u8).initCapacity(alloc, 1024);
 
-    var ops = lineIts.pop() orelse return error.MissingOps;
+        var values = mem.tokenizeScalar(u8, line, ' ');
 
-    var total: i32 = 0;
-
-    for (0..4) |_| {
-        var nums = try ArrayList(i32).initCapacity(alloc, 1024);
-        defer nums.deinit(alloc);
-
-        for (lineIts.items) |*it| {
-            if (it.next()) |item| {
-                const num = try std.fmt.parseInt(i32, item, 10);
-                try nums.append(alloc, num);
-            }
+        while (values.next()) |value| {
+            try line_slice.append(alloc, value);
         }
 
-        if (ops.next()) |op| {
-            if (mem.eql(u8, op, "+")) {
-                total += add(nums);
-            } else if (mem.eql(u8, op, "*")) {
-                total += multiply(nums);
-            }
+        try line_slices.append(alloc, line_slice);
+    }
+
+    var op_slice = line_slices.pop() orelse return error.MissingOps;
+    defer op_slice.deinit(alloc);
+
+    var total: u64 = 0;
+
+    for (0..op_slice.items.len) |index| {
+        var nums_slice = try ArrayList(u64).initCapacity(alloc, 1024);
+        defer nums_slice.deinit(alloc);
+
+        for (line_slices.items) |slice| {
+            const value = slice.items[index];
+            const num = try fmt.parseInt(u64, value, 10);
+            try nums_slice.append(alloc, num);
+        }
+
+        const op = op_slice.items[index];
+
+        if (mem.eql(u8, op, "+")) {
+            total += add(nums_slice);
+        } else if (mem.eql(u8, op, "*")) {
+            total += multiply(nums_slice);
         }
     }
 
+    return total;
+}
+
+test "test" {
+    var gpa = GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const alloc = gpa.allocator();
+
+    const buf = try readFile(alloc, "test.txt");
+    defer alloc.free(buf);
+
+    const total = try sum(alloc, buf);
+
+    try std.testing.expectEqual(4277556, total);
+}
+
+pub fn main() !void {
+    var gpa = GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const alloc = gpa.allocator();
+
+    const buf = try readFile(alloc, "input.txt");
+    defer alloc.free(buf);
+
+    const total = try sum(alloc, buf);
     std.debug.print("{d}\n", .{total});
 }
